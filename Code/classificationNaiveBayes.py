6133019e94047ce3a0227ef75b924d0143c2db2f
elena-krismer <elena.krismer@hotmail.com>
2021-04-13 20:16:52 +0200

    confusion matrix

--- Code/classificationNaiveBayes.py
+++ Code/classificationNaiveBayes.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.metrics import confusion_matrix
 
 ########################
 # Supervised Learning  #
@@ -36,9 +37,14 @@ nb_clf.fit(X_train, y_train)
 # Apply trained model to make prediction
 y_pred = nb_clf.predict(X_test)
 
-print("Classification - Naive Bayes")
-print("Accuracy:", nb_clf.score(X_test, y_test))
-print("Mean Absolute Error:", metrics.mean_absolute_error(y_test, y_pred))
-print("Mean Squared Error:", metrics.mean_squared_error(y_test, y_pred))
-print("Root Mean Absolute Error:", np.square(metrics.mean_absolute_error(y_test, y_pred)))
+# Confusion Matrix, Sensitivity and Specifity
+true_neg, false_pos, false_neg, true_pos = confusion_matrix(y_test, y_pred).ravel()
+specificity = true_neg / (true_neg + false_pos)
+sensitivity = true_pos / (true_pos + false_neg)
 
+print("Classification - Naive Bayes \n\nAccuracy:", nb_clf.score(X_test, y_test),
+      "\nSpecificity:", specificity, "\nSensitivity:", sensitivity,
+      "\n\nMean Absolute Error:", metrics.mean_absolute_error(y_test, y_pred),
+      "\nMean Squared Error:", metrics.mean_squared_error(y_test, y_pred),
+      "\nRoot Mean Absolute Error:",
+      np.square(metrics.mean_absolute_error(y_test, y_pred)))
