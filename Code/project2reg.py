040f9345e0c21be644bdaffdfbe8284abc59ce0a
kostaspalia <73672970+kostaspalia@users.noreply.github.com>
2021-04-19 13:24:06 +0200

    added t-test  , code  works but may need cleanup

--- Code/project2reg.py
+++ Code/project2reg.py
@@ -12,6 +12,7 @@ from sklearn import model_selection
 from toolbox_02450 import train_neural_net, draw_neural_net, visualize_decision_boundary,rlr_validate
 import torch
 import sklearn.linear_model as lm
+import numpy as np, scipy.stats as st
 
 filename= '../Data/SAheart.csv'
 df = pd.read_csv(filename)
@@ -84,6 +85,10 @@ arrayH = []
 arrayL = []
 valerrors = np.zeros([S,K2])
 
+basepredict = np.empty(len(ylin))
+linregpredict = np.empty(len(ylin))
+ANNpredict = np.empty(len(ylin))
+
 CVouter = model_selection.KFold(K1,shuffle=True)
 for (k1, (par_index, test_index)) in enumerate(CVouter.split(X,y)): 
     print('\nCrossvalidation fold: {0}/{1}'.format(k1+1,K1))    
@@ -107,6 +112,7 @@ for (k1, (par_index, test_index)) in enumerate(CVouter.split(X,y)):
     
     arrayEbase.append(Error_test[k1][0])
     
+    basepredict[test_index]=m.predict(X_test)
     ############# linear regression ###############
     opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_par, y_par, lambdas,K2)
 
@@ -120,6 +126,7 @@ for (k1, (par_index, test_index)) in enumerate(CVouter.split(X,y)):
     Xty = X_par.T @ y_par
     XtX = X_par.T @ X_par
     
+    
 
     # Estimate weights for the optimal value of lambda, on entire training set
     lambdaI = opt_lambda * np.eye(M)
@@ -131,6 +138,8 @@ for (k1, (par_index, test_index)) in enumerate(CVouter.split(X,y)):
     
     arrayElr.append(Error_test_rlr[k1][0])
     arrayL.append(opt_lambda)
+    
+    linregpredict[test_index]=X_test @ w_rlr[:,k1]
     
     ######### neural network ##########
     CVinner=model_selection.KFold(K2,shuffle=True)   
@@ -175,6 +184,7 @@ for (k1, (par_index, test_index)) in enumerate(CVouter.split(X,y)):
     # Predict values on test set
     y_test_est = net(X_testTensor)
     
+    ANNpredict[test_index]=np.squeeze(np.asarray(y_test_est.data.numpy()))
     # Determine errors and errors
     se = (y_test_est.float()-y_testTensor.float())**2 # squared error
     Etest = (sum(se).type(torch.float)/len(y_test)).data.numpy() #mean
@@ -188,4 +198,31 @@ print("\nh = {}".format(arrayH))
 print("EtestANN = {}".format(arrayEANN))
 print("\nλ = {}".format(arrayL))
 print("Etest linear regression = {}".format(arrayElr))
-print("Etest baseline = {}".format(arrayEbase))        
+print("Etest baseline = {}".format(arrayEbase))  
+
+zANN = np.abs(ylin - ANNpredict ) ** 2
+zbase = np.abs(ylin - basepredict ) ** 2
+zlinreg = np.abs(ylin - linregpredict ) ** 2
+
+
+# Compute confidence interval p-value of Null hypothesis
+alpha=0.05
+z1 = zANN - zlinreg
+CI1 = st.t.interval(1-alpha, len(z1)-1, loc=np.mean(z1), scale=st.sem(z1))  # Confidence interval
+p1 = st.t.cdf( -np.abs( np.mean(z1) )/st.sem(z1), df=len(z1)-1)  # p-value
+
+print("zeta1 = ANN-linear regression ---->", " CI= ", CI1, "p-value=", p1)
+
+z2 = zANN - zbase
+CI2 = st.t.interval(1-alpha, len(z2)-1, loc=np.mean(z2), scale=st.sem(z2))  # Confidence interval
+p2 = st.t.cdf( -np.abs( np.mean(z2) )/st.sem(z2), df=len(z2)-2)  # p-value
+
+print("zeta2 = ANN-baseline ---->"," CI= ", CI2, "p-value=", p2)
+
+z3 = zlinreg - zbase
+CI3 = st.t.interval(1-alpha, len(z3)-1, loc=np.mean(z3), scale=st.sem(z3))  # Confidence interval
+p3 = st.t.cdf( -np.abs( np.mean(z3) )/st.sem(z3), df=len(z3)-1)  # p-value
+
+print("zeta3 = linear regression-baseline ---->", " CI= ", CI3, "p-value=", p3)
+
+      
